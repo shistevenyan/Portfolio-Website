3c692e44e649ff47d16062e1d3fdef5b8c672c3f
Steven Yan <ssyan@ualberta.ca>
2021-01-24 19:48:50 -0700

    added some animations

--- steven-portfolio/src/App.js
+++ steven-portfolio/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Typical from 'react-typical';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
+        <h1> Welcome <span className="wave" >👋</span>😄 </h1>
+                 
+        <Typical
+          loop={1}
+          wrapper='h4'
+          steps={['My name is Steven Yan.']}
+        />
+
       </header>
     </div>
   );
