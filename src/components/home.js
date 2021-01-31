ec14a5921975c82622f26e18ca7a67f4a4821a9e
Steven Yan <ssyan@ualberta.ca>
2021-01-30 18:17:18 -0700

    change file locations

---
+++ src/components/home.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import '../styles/home.scss';
+import { Link } from "react-scroll";
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Fade from 'react-reveal/Fade';
+
+class Home extends Component {
+    state = {};
+    render() {
+        return (
+            <Container className="home-section" id="home" fluid={true}>
+                <Row>
+                    <Col md={12}>
+                        <Fade>
+                            <h1>Hey, how's it going? <span className="wave" >👋</span>😄</h1>
+                            <br></br>
+                        </Fade>
+                    </Col>
+                </Row>
+                <Row>
+                    <Fade left delay={1000}>
+                        <Col md={12}>
+                            <h2>My name is Steven Yan. 🧍‍♂️</h2>
+                            <br></br>
+                        </Col>
+                    </Fade>
+                </Row>
+                <Row>
+                    <Fade bottom delay={2200}>
+                        <Col md={12}>
+                            <p className="home-context">
+                                I am a Software Engineer based in Edmonton, Alberta. I recently graduated from the University of Alberta
+                                in Computer Science and am currently looking for new opportunities. I have a passion for UI/UX Design and
+                                Front-End Development. When designing interfaces, I prefer a minimalistic style, as you can probably tell. When
+                                relaxing, I enjoy playing video games (Genshin Impact or Valorant) and cooking.
+                            <br />
+                                <br />
+                            Come check out what I've been doing! 👀
+                            <br />
+                                <br />
+                            </p>
+
+                            <Link
+                                activeClass="active"
+                                to="projects"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                            >
+                                <button className="scrollProject">See Projects</button>
+                            </Link>
+                        </Col>        
+                    </Fade>
+                </Row>
+            </Container>
+            
+
+        );
+    }
+
+}
+
+export default Home;
---
+++ src/components/projects.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import '../styles/projects.scss';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Fade from 'react-reveal/Fade';
+import { Link } from "react-scroll";
+import { VscLinkExternal } from "react-icons/vsc";
+
+class Projects extends Component {
+    state = {};
+    render() {
+        return (
+            <Container className="projects-section" id="projects" fluid={true}>
+                <Row>
+                    <Col className="project-space">
+                        <div></div>
+                    </Col>
+                    <Col sm={12}>
+                        <Fade delay={200}>
+                            <h2>Projects 🖥️</h2>
+                        </Fade>
+                    </Col> 
+                </Row>
+                <Row>
+                    <Col sm={4}>
+                        <Fade bottom delay={400}>
+                            <br></br>
+                            <h4>PeppeR Recommender</h4>
+                            <p className="projects-context">
+                                The PeppeR Recommender is a discussion recommender for the PeppeR Project, a collaborative learning environment 
+                                for students. I collaborated with Dr. Carrie Demmans Epp and the EdTeKLA Research Group from the University of 
+                                Alberta. This project was made using a LAMP stack (Linux, Apache, MySQL, PHP) environment.
+                            </p>
+                            <a href="https://github.com/shistevenyan" target="_blank" className="projectLink"><VscLinkExternal /></a>
+                            <br></br>
+                        </Fade>
+                    </Col>
+
+                    <Col sm={4}>
+                        <Fade bottom delay={600}>
+                            <br></br>
+                            <h4>YeggerStop</h4>
+                            <p className="projects-context">
+                                YeggerStop is a web application for the Edmonton Transit System. YeggerStop tells you the nearby bus stops along with their 
+                                respective bus routes and arrival times. Currently, this project is a Work in Progress and only displays nearby 
+                                bus stops. This project is coded using a LAMP stack (Linux, Apache, MySQL, PHP) environment.
+                            </p>
+                            <a href="https://github.com/shistevenyan" target="_blank" className="projectLink"><VscLinkExternal /></a>
+                            <br></br>
+                        </Fade>
+                    </Col>
+
+                    <Col sm={4}>
+                        <Fade bottom delay={800}>
+                            <br></br>
+                            <h4>Fooend</h4>
+                            <p className="projects-context">
+                                Fooend is an Android app that connects users with similar tastes in food and allows them to meet up for a meal. Currently, 
+                                this project is a Work in Progress with some fellow University of Alberta CS classmates. It is currently in the design phase on 
+                                Figma. This project will be coded using the Android Studio environment. Look forward to new updates in mid-2021.
+                            </p>
+                            <a href="https://github.com/shistevenyan" target="_blank" className="projectLink"><VscLinkExternal /></a>
+                        </Fade>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Fade bottom delay={1200}>
+                            <br></br>
+                            <br></br>
+                            <Link
+                                activeClass="active"
+                                to="contact"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                            >
+                                <button className="scrollContact">Contact Me</button>
+                            </Link>
+                        </Fade>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+
+}
+
+export default Projects;
